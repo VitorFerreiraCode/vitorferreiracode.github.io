c65bd6473cd2272535b5162f3c675d8bd060cd73
Vitor Ferreira <vitorferreiraviana1703@gmail.com>
2025-01-20 19:03:39 -0300

    alterações

---
+++ script.js
@@ -0,0 +1,40 @@
+document.querySelector('.profile-picture').addEventListener('mouseenter', function() {
+    const originalSrc = this.src;
+    const hoverSrc = this.getAttribute('data-hover');
+    this.src = hoverSrc;
+    
+    this.addEventListener('mouseleave', function onMouseLeave() {
+        this.src = originalSrc;
+        this.removeEventListener('mouseleave', onMouseLeave);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+    const menuLinks = document.querySelectorAll('.header-nav a');
+
+    menuToggle.addEventListener('click', function() {
+        menuToggle.classList.toggle('active');
+        nav.classList.toggle('active');
+        document.body.classList.toggle('menu-open');
+    });
+
+    // Fecha o menu ao clicar em um link
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menuToggle.classList.remove('active');
+            nav.classList.remove('active');
+            document.body.classList.remove('menu-open');
+        });
+    });
+
+    // Fecha o menu ao redimensionar a tela para desktop
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 768) {
+            menuToggle.classList.remove('active');
+            nav.classList.remove('active');
+            document.body.classList.remove('menu-open');
+        }
+    });
+});
